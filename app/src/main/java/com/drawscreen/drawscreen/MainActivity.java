8bcd90669bcc21e7fe869e15a8cf9ebff1fbf8ff
Unknown <rootn66@gmail.com>
2018-05-16 15:56:00 -0500

    add permissions
    add one permission

--- app/src/main/java/com/drawscreen/drawscreen/MainActivity.java
+++ app/src/main/java/com/drawscreen/drawscreen/MainActivity.java
@@ -1,9 +1,12 @@
 package com.drawscreen.drawscreen;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,8 +16,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_ASK_PERMISSIONS = 123;
     private ListView lvDrwas;
     private FileDraw fileDraw;
     @Override
@@ -23,13 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lvDrwas = (ListView)findViewById(R.id.lvDraws);
         fileDraw = new FileDraw();
-        loadDraw();
-        lvDrwas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                runDraw(String.valueOf(lvDrwas.getItemAtPosition(position)));
-            }
-        });
+        checkPermission();
     }
 
     @Override
@@ -76,11 +75,59 @@ public class MainActivity extends AppCompatActivity {
     private void loadDraw(){
         ArrayAdapter <String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,fileDraw.getDraws());
         lvDrwas.setAdapter(adapter);
+        lvDrwas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                runDraw(String.valueOf(lvDrwas.getItemAtPosition(position)));
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         loadDraw();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(REQUEST_CODE_ASK_PERMISSIONS == requestCode) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "OK Permissions granted ! :-) " + Build.VERSION.SDK_INT, Toast.LENGTH_LONG).show();
+                loadDraw();
+            } else {
+                Toast.makeText(this, "Permissions are not granted ! :-( " + Build.VERSION.SDK_INT, Toast.LENGTH_LONG).show();
+            }
+        }else{
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    private void checkPermission() {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+
+            Toast.makeText(this, "This version is not Android 6 or later " + Build.VERSION.SDK_INT, Toast.LENGTH_LONG).show();
+
+        } else {
+
+            int hasWriteContactsPermission = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+
+                Toast.makeText(this, "Requesting permissions", Toast.LENGTH_LONG).show();
+
+            }else if (hasWriteContactsPermission == PackageManager.PERMISSION_GRANTED){
+
+                Toast.makeText(this, "The permissions are already granted ", Toast.LENGTH_LONG).show();
+                loadDraw();
+
+            }
+
+        }
+
+        return;
     }
 }
